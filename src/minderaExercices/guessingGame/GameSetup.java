00908f5110ce5d02854793d576f9851d4981d9b4
Gabriel <gabspereira178@gmail.com>
2024-11-10 14:12:31 +0000

    initialize scanner, change playround logic to utilize difficulty and has a to pass it down to the new methods that handle bot and user input seperatly in those i will be cheking, playerguess agains array of guesses and handling if they won that round this is also a method to be able to seperate concerns
    Signed-off-by: Gabriel <gabspereira178@gmai.com>

--- src/minderaExercices/guessingGame/GameSetup.java
+++ src/minderaExercices/guessingGame/GameSetup.java
@@ -1,10 +1,14 @@
 package minderaExercices.guessingGame;
 
+import java.util.Scanner;
+
 public class GameSetup {
 	private static int initialPlayersNumber;
 	//instance in gamesetup rather than in players so all will play the same game
 	//otherwise each would play its individual game
 	private final Game game = new Game();
+	private GameScanner scanner;
+	private Difficulty difficulty;
 
 	public GameSetup(int initialPlayersNumber) {
 		GameSetup.initialPlayersNumber = initialPlayersNumber;
@@ -36,17 +40,17 @@ public class GameSetup {
 
 		initializePlayers(this.initialPlayersNumber, game);
 
-		gameLogic();
+		gameLogic(difficulty);
 	}
 
-	private void gameLogic() {
+	private void gameLogic(Difficulty difficulty) {
 
 		//need to initialize the fisrt game number so the rounds can really start
 		game.generateGameNumber();
 
 		boolean gameWon = false;
 		while (!gameWon) {
-			playRound();
+			playRound(difficulty);
 			for (Player player : Player.getPlayers()) {
 				if (player.getWinningCounter() == 3) {
 					System.out.printf("\033[32m%s has won the game with 3 wins!\033[0m\n", player.getPlayerName());
@@ -57,29 +61,17 @@ public class GameSetup {
 		}
 	}
 
-	private void playRound() {
+	private void playRound(Difficulty difficulty) {
 
-		System.out.printf("\033[41mThe game number is %s\033[0m\n",
-				game.getRandomNumberGenerated());
+		System.out.printf("\033[41mThe game number range is %s and %s\033[0m\n",
+				difficulty.getMinRange(), difficulty.getMaxRange());
 
 		boolean win = false;
 		while (!win) {
-			for (int i = 0; i < Player.getPlayerCount(); i++) {
-				//to access each player grab that form the array players
-				Player currentPlayer = Player.getPlayers()[i];
-				int currentPlayerGuess = currentPlayer.generatePlayerGuess();
-
-				if (isCorrectGuess(currentPlayerGuess)) {
-					System.out.printf("\033[34m%s wins this round\033[0m\n",
-							currentPlayer.getPlayerName());
-					currentPlayer.addingWinning();
-					currentPlayer.resetPlayerGuesses();
-					currentPlayer.resetGuessesCount();
-					game.generateGameNumber();
-					win = true;
-					break;
-				}
-
+			for (Player player : Player.getPlayers()) {
+				win = player.getPlayerName().equals("Player1") ? handleUserTurn(player, difficulty) :
+						handleBotTurn(player);
+				if (win) break;
 			}
 		}
 	}
@@ -88,4 +80,32 @@ public class GameSetup {
 		return currentPlayerGuess == game.getRandomNumberGenerated();
 	}
 
+	private boolean handleUserTurn(Player player, Difficulty difficulty) {
+
+		scanner = new GameScanner();
+		scanner.handleUserInput(difficulty);
+
+		int userGuess = scanner.getUserFinalChoice();
+		player.addGuess(userGuess);
+		return handleWin(player, userGuess);
+	}
+
+	private boolean handleBotTurn(Player player) {
+		int currentPlayerGuess = player.generatePlayerGuess();
+		System.out.println("\033[36m" + player.getPlayerName() + " has guessed " + currentPlayerGuess + "\033[0m");
+		return handleWin(player, currentPlayerGuess);
+	}
+
+	private boolean handleWin(Player player, int currentPlayerGuess) {
+		if (isCorrectGuess(currentPlayerGuess)) {
+			System.out.printf("\033[34m%s wins this round\033[0m\n",
+					player.getPlayerName());
+			player.addingWinning();
+			player.resetPlayerGuesses();
+			player.resetGuessesCount();
+			game.generateGameNumber();
+			return true;
+		}
+		return false;
+	}
 }
