652ab39eb55d05adf150ac20bed35a1d89f6a952
Gabriel <gabspereira178@gmail.com>
2024-11-13 11:02:32 +0000

    testing project need some prints to see changes in balance
    Signed-off-by: Gabriel <gabspereira178@gmail.com>

--- src/minderaExercices/Bank/Main.java
+++ src/minderaExercices/Bank/Main.java
@@ -1,9 +1,6 @@
 package minderaExercices.Bank;
 
-import minderaExercices.Bank.bank.Bank;
-import minderaExercices.Bank.bank.CreditAccount;
-import minderaExercices.Bank.bank.DebitAccount;
-import minderaExercices.Bank.bank.DebitCard;
+import minderaExercices.Bank.bank.*;
 
 public class Main {
 	public static void main(String[] args) {
@@ -12,30 +9,9 @@ public class Main {
 		CreditAccount creditTest = bankTest.createCreditAccount("1234");
 		DebitAccount debitTest = bankTest.createDebitAccount("1234");
 		bankTest.askCard(debitTest.getIban(), "1234");
-		bankTest.askCard(creditTest.getIban(), "1234");
-
-		bankTest.createCustomer("cris","231");
-		CreditAccount creditTest2 = bankTest.createCreditAccount("231");
-		DebitAccount debitTest2 = bankTest.createDebitAccount("231");
-		bankTest.askCard(debitTest2.getIban(), "231");
-		bankTest.askCard(creditTest2.getIban(), "231");
-
-
-		//!!must spit the new account iban that will be generated with ++ from to 0 - infinty
-
-
-		//?? always check for size of arrays cardNumbers and accounts and see if the counter that i use to
-		//?? go trough them is bigger then the arrays length
-		//!bank.askCard(accountIban)
-			//!go to all bank accts, check which account id matchs with the accountIban if there are any
-			//! generate newCardId can be a counter ++
-			//! then go to the account cardId and add to it based on THIS account cardIdCounter
-			//! everytime i want to add a new cardid i can simply go to cardId[cardIdCounter]
-			//!spit card number id
-
-		//!bank.widhdraw(cardNumberId, amount)
-			//!go to all account find an account that within his cardId array has that card number
-			//! get that account balance and subtract amount passed trough the method
-
+		int creditCardid = bankTest.askCard(creditTest.getIban(), "1234");
+		bankTest.deposit(creditCardid, "1234", 100);
 	}
+
+	//!add prints to say if money has been deposit like previous balance and newbalance maybe
 }
