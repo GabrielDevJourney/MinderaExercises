5af5762d73fb406bc2df6f559b86875667e40229
Gabriel <gabspereira178@gmail.com>
2024-11-19 07:25:48 +0000

    change ranom change of bite to call the generative method from random class
    Signed-off-by: Gabriel <gabspereira178@gmail.com>

--- src/minderaExercices/MonsterGame/Monsters/Vampire.java
+++ src/minderaExercices/MonsterGame/Monsters/Vampire.java
@@ -1,5 +1,7 @@
 package minderaExercices.MonsterGame.Monsters;
 
+import minderaExercices.MonsterGame.Random;
+
 public class Vampire extends Monster {
 
 	private int healthBack = 15;
@@ -10,9 +12,9 @@ public class Vampire extends Monster {
 		damage = 18;
 	}
 
-	//to utilize in game i must return if vampire will bite or not
+	//to utilize in game i must return if vampire will bite or not so the action will ocure in game
 	public boolean bite() {
-		int changeOfBite = generateChangeOfBite();
+		int changeOfBite = Random.generateBiteChange();
 
 		//25% change of biting, also cheking if health is near 85 ensure that health doesn't go above is max health
 		if (changeOfBite <= 25 && health <= 85) {
@@ -22,7 +24,4 @@ public class Vampire extends Monster {
 		return false;
 	}
 
-	private int generateChangeOfBite() {
-		return (int) (Math.random() * 100 + 1);
-	}
 }
